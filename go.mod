c4203641ea762cbe32a16f9746baa2b3cf470e90
t-kikuc <tkikuchi07f@gmail.com>
2024-05-09 17:32:50 +0900

    Impl the command
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- go.mod
+++ go.mod
@@ -2,9 +2,33 @@ module export-ecsdef
 
 go 1.21.3
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.12
+	github.com/aws/aws-sdk-go-v2/service/ecs v1.41.8
+	github.com/goccy/go-yaml v1.11.3
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.12 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.7 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/goccy/go-yaml"
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +43,149 @@ func newCommand() *cobra.Command {
 	return cmd
 }
 
+// run controls the main flow of the command.
 func (o *options) run(ctx context.Context) error {
+	client, err := newDefaultECSClient()
+	if err != nil {
+		return fmt.Errorf("failed to create ECS client: %v", err)
+	}
+
+	serviceArns, err := client.ListServices(ctx, o.cluster)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Found %d services\n", len(serviceArns))
+
+	for i, serviceArn := range serviceArns {
+		service, err := client.DescribeService(ctx, o.cluster, serviceArn)
+		if err != nil {
+			return err
+		}
+
+		taskDef, err := client.DescribeTaskDefinition(ctx, *service.TaskDefinition)
+		if err != nil {
+			return err
+		}
+
+		formatService(service)
+		formatTaskDefinition(taskDef)
+
+		if err := o.export(service, taskDef); err != nil {
+			return err
+		}
+
+		fmt.Printf(" %d. Export succeeded: %s\n", (i + 1), *service.ServiceName)
+	}
+
+	fmt.Println("Successfully finished exporting.")
 	return nil
+}
+
+// export exports the service and the taskDefinition as yaml files.
+// The file names are {outdir}/{serviceName}/servicedef.yaml and {outdir}/{serviceName}/taskdef.yaml.
+func (o *options) export(service *types.Service, taskdef *types.TaskDefinition) error {
+	// create a directory
+	dir := fmt.Sprintf("%s/%s", o.outdir, *service.ServiceName)
+	if err := os.Mkdir(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create a directory %s: %w", *service.ServiceName, err)
+	}
+
+	// Service
+	svcBytes, err := yaml.Marshal(service)
+	if err != nil {
+		return fmt.Errorf("failed to marshal service %s: %w", *service.ServiceName, err)
+	}
+	svcPath := fmt.Sprintf("%s/servicedef.yaml", dir)
+	if err := os.WriteFile(svcPath, svcBytes, 0644); err != nil {
+		return fmt.Errorf("failed to write service yaml file of %s: %w", *service.ServiceName, err)
+	}
+
+	// Task Definition
+	tdBytes, err := yaml.Marshal(taskdef)
+	if err != nil {
+		return fmt.Errorf("failed to marshal taskDefinition %s: %w", *taskdef.Family, err)
+	}
+	tdPath := fmt.Sprintf("%s/taskdef.yaml", dir)
+	if err := os.WriteFile(tdPath, tdBytes, 0644); err != nil {
+		return fmt.Errorf("failed to write taskDefinition yaml file of %s: %w", *taskdef.Family, err)
+	}
+
+	return nil
+}
+
+// formatService removes unnecessary fields.
+func formatService(service *types.Service) {
+	service.CreatedAt = nil
+	service.CreatedBy = nil
+	service.Deployments = nil
+	service.Events = nil
+
+	service.LoadBalancers = nil
+
+	service.PendingCount = 0
+	service.RunningCount = 0
+	service.Status = nil
+	service.TaskSets = nil
+}
+
+// formatTaskDefinition removes unnecessary fields.
+func formatTaskDefinition(taskDef *types.TaskDefinition) {
+	taskDef.DeregisteredAt = nil
+	taskDef.RegisteredAt = nil
+	taskDef.RegisteredBy = nil
+
+	// taskDef.Status = nil // TODO: how to remove this field?
+}
+
+type ecsClient struct {
+	client *ecs.Client
+}
+
+// newDefaultECSClient creates a new ECS Client with SDK's default configuration.
+func newDefaultECSClient() (*ecsClient, error) {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		fmt.Printf("unable to load SDK's default config, %v", err)
+		return nil, err
+	}
+
+	return &ecsClient{client: ecs.NewFromConfig(cfg)}, nil
+}
+
+// ListServices lists services in the cluster.
+func (c *ecsClient) ListServices(ctx context.Context, cluster string) ([]string, error) {
+	res, err := c.client.ListServices(ctx, &ecs.ListServicesInput{
+		Cluster: &cluster,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res.ServiceArns, nil
+}
+
+// DescribeService describes the service.
+func (c *ecsClient) DescribeService(ctx context.Context, cluster, service string) (*types.Service, error) {
+	res, err := c.client.DescribeServices(ctx, &ecs.DescribeServicesInput{
+		Cluster:  &cluster,
+		Services: []string{service},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(res.Services) == 0 {
+		return nil, fmt.Errorf("service %s not found", service)
+	}
+	return &res.Services[0], nil
+}
+
+// DescribeTaskDefinition describes the taskDefinition.
+func (c *ecsClient) DescribeTaskDefinition(ctx context.Context, taskDefArn string) (*types.TaskDefinition, error) {
+	res, err := c.client.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: &taskDefArn,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res.TaskDefinition, nil
 }
